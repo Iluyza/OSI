ab008bd14b9c30cdc4200cbbd027f3adf6a7e89c
Iluyza <90064161+Iluyza@users.noreply.github.com>
2023-12-03 21:23:55 +0300

    Update child.py

--- task2/child.py
+++ task2/child.py
@@ -3,12 +3,14 @@ import sys
 import time
 import random
 
-s = int(sys.argv[1])
 pid = os.getpid()
 ppid = os.getppid()
 
 print(f'Child[{pid}]: I am started. My PID {pid}. Parent PID {ppid}.')
+
+s = int(sys.argv[1])
 time.sleep(s)
 print(f'Child[{pid}]: I am ended. PID {pid}. Parent PID {ppid}.')
 
-sys.exit(random.randint(0, 1))
+exit_status = random.choice([0, 1])
+os._exit(exit_status)

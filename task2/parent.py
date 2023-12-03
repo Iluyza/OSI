09e61f69f9db0e5ee38e3155c6d84ee6cc60731d
Iluyza <90064161+Iluyza@users.noreply.github.com>
2023-12-03 21:51:10 +0300

    Update parent.py

--- task2/parent.py
+++ task2/parent.py
@@ -1,18 +1,29 @@
-from os import fork, getpid, wait, execv
-from sys import argv
-from random import randint
+import os
+import random
+import sys
 
-n = int(argv[1])
-
-for i in range(n):
-    child = fork()
+def start_child_process():
+    child = os.fork()
     if child == 0:
-        number = randint(5, 10)
-        print(f"Parent[{getpid()}]: I ran children process with PID {getpid()}.")
-        execv('/usr/bin/python3', ['python3', 'child.py', str(number)])
+        sleep_time = random.randint(5, 10)
+        os.execve('/usr/bin/python3', ['/usr/bin/python3', 'child.py', str(sleep_time)], os.environ)
     else:
-        ret = wait()
-        if ret[1] == 0:
-            print(f"Parent[{getpid()}]: Child with PID {ret[0]} terminated. Exit Status {ret[1]}.")
-        else:
-            print(f"Parent[{getpid()}]: Child with PID {ret[0]} terminated. Exit Status {ret[1]}. Starting a new child process.")
+        print(f'Parent[{os.getpid()}]: I ran children process with PID {child}')
+
+n = int(sys.argv[1])
+
+for _ in range(n):
+    start_child_process()
+
+counter = 0
+
+while counter != n:
+    child_pid, exit_status = os.wait()
+    print(f'Parent[{os.getpid()}]: Child with PID {child_pid} terminated. Exit status {os.WEXITSTATUS(exit_status)}')
+    
+    if exit_status != 0:
+        start_child_process()
+    else:
+        counter += 1
+
+os._exit(0)
